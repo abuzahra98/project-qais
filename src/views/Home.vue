1490fb14806202fbc29339a526507781fbee71c7
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2023-06-27 23:15:22 +0300

    fix

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <SectionOne />
+  <SectionTow />
+</template>
+
+<script setup>
+  import SectionOne from '@/components/SectionOne.vue'
+  import SectionTow from '@/components/SectionTow.vue'
+</script>
